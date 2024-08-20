8ed69427f3d8a2ac831ffd9675e48fb11bdd3337
kenkairon <ingeinfo2006@gmail.com>
2024-08-20 16:14:39 -0400

    cambio de imagenes y script de jquery con el objetivo que apunte al id del elemento clicado

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,11 @@
 $(document).ready(function() {
+  // Manejar clics en elementos con la clase .text-body-secondary
   $(".text-body-secondary").click(function() {
-    var idBoton = $("#Rio").attr("id");
-    $("#detalles" + idBoton).toggle(); // Se utiliza correctamente el valor de idBoton
+    var idBoton = $(this).attr("id"); // Obtiene el id del elemento clicado
+    $("#detalles" + idBoton).toggle(); // Muestra/oculta el elemento correspondiente
   });
 
+  // Manejar clics en elementos con la clase .btn-close
   $(".btn-close").click(function() {
     $(".detalles").hide(); // Oculta todos los elementos con la clase "detalles"
   });
